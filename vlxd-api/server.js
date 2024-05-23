0eef4982325bc0cfbead65c2d89835313b060b22
LeCheWang <haphonggoroki@gmail.com>
2024-05-23 22:44:15 +0700

    fix cors

--- vlxd-api/server.js
+++ vlxd-api/server.js
@@ -7,7 +7,26 @@ const configuration = require('./configs/configuration');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// Danh sách các origin được phép
+const allowedOrigins = [
+  'http://45.77.32.9:3001', 
+  'https://45.77.32.9:3001', 
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Kiểm tra nếu origin nằm trong danh sách các origin được phép
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 router(app);
 connectDB();
